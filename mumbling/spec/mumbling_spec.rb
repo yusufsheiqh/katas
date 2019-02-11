858c7eca91c8a35d3b7544f1cf68f084e0c5ccd0
Yusuf <yusuf@madetech.com>
2019-02-11 11:38:57 +0000

    Add mumbling kata solution

---
+++ mumbling/spec/mumbling_spec.rb
@@ -0,0 +1,35 @@
+class Mumbler
+    def accum(string)
+        string.chars.map.with_index { |char, index| (char * (index + 1)).capitalize }.join('-')
+    end
+end
+
+describe Mumbler do
+    it 'returns an empty string' do
+        expect(subject.accum('')).to eq('')
+    end
+
+    it 'returns A when input string is a' do
+        expect(subject.accum('a')).to eq('A')
+    end
+
+    it 'returns B when input string is b' do
+        expect(subject.accum('b')).to eq('B')
+    end
+
+    it 'returns C when input string is c' do
+        expect(subject.accum('c')).to eq('C')
+    end
+
+    it 'returns A-Bb when input string is ab' do
+        expect(subject.accum('ab')).to eq('A-Bb')
+    end
+
+    it 'returns A-Bb-Ccc when input string is abc' do
+        expect(subject.accum('abc')).to eq('A-Bb-Ccc')
+    end
+
+    it 'returns R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy when input string is RqaEzty' do
+        expect(subject.accum('RqaEzty')).to eq('R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy')
+    end
+end
--- tennis/spec/tennis_spec.rb
+++ tennis/spec/tennis_spec.rb
@@ -1,3 +1,8 @@
 describe Tennis do
-    
+    it "returns a score of 0-0 as game begins" do
+        game = Tennis.new
+
+        p1_score = game.p1_score
+        p2_score = game.p2_score
+    end
 end
