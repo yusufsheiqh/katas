514d55e715b7f0f73deedb5ef6875c2c926b03be
Yusuf <yusuf@madetech.com>
2019-02-11 11:49:49 +0000

    Add spec file for tennis kata

--- tennis/spec/tennis_spec.rb
+++ tennis/spec/tennis_spec.rb
@@ -2,7 +2,7 @@ describe Tennis do
     it "returns a score of 0-0 as game begins" do
         game = Tennis.new
 
-        p1_score = game.p1_score
         p2_score = game.p2_score
+        p1_score = game.p1_score
     end
 end
